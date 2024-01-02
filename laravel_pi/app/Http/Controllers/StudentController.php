3484b2390d034636f76a8c68e1c35474fc6b47c1
Bartłomiej Pawlik <148866688+Lacquerri@users.noreply.github.com>
2024-01-03 00:16:00 +0100

    1/2

---
+++ laravel_pi/app/Http/Controllers/StudentController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\Student;
+
+class StudentController extends Controller
+{
+    public function store(Request $request){
+        $data = $request->validate([
+            'Imie' => 'required',
+            'Nazwisko' => 'required',
+            'Ocena' => 'required',
+        ]);
+        $newStudent = Student::create($data);
+        return view('welcome');
+    }
+
+    public function read(){
+        $students = Student::all();
+        return view('student', ['students' => $students]);
+    }
+
+    public function update(Student $student){
+        return view('update', ['student' => $student]);
+    }
+
+    public function dbupdate(Student $student, Request $request){
+        $data = $request->validate([
+            'Imie' => 'required',
+            'Nazwisko' => 'required',
+            'Ocena' => 'required',
+        ]);
+        $student->update($data);
+        return redirect(route('student.read'));
+    }
+      public function delete(Student $student){
+        $student->delete();
+        return redirect(route('student.read'));
+    }
+}
